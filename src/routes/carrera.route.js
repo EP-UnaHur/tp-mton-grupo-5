e8aba6bccf7e468dd32b9cf4cc81cf959f668a30
LucasHerreraNieto <lucassantiago.herreranieto@estudiantes.unahur.edu.ar>
2024-06-02 17:43:11 -0300

    correcciones

--- src/routes/carrera.route.js
+++ src/routes/carrera.route.js
@@ -4,11 +4,7 @@ const {Carreras} = require('../db/models')
 const validarSchema =require('../middlewares/validateSchema.middleware')
 const existeId = require('../middlewares/exists.middleware')
 const carreraSchema = require('../schemas/carrera.schema')
-const materiaSchema = require('../schemas/materia.schema')
-
 const carreraController = require('../controllers/carrera.controller')
-
-const middlewareCarrera = require('../middleware/existe.middleware')
 const carreraSchema = require('../schemas/carrera.schema')
 
 //Obtengos todas las carreras
@@ -18,15 +14,11 @@ router.get('/carreras', carreraController.getAllCarreras)
 router.get('/carreras/:id', existeId.existsById(Carreras),carreraController.carreraById)
 
 //Crear una carrera
-<<<<<<< HEAD
 router.post('/carreras', validarSchema.validaSchema(carreraSchema),carreraController.crearCarrera)
 
 //Obtengo las materias de una carrera
 router.get('/carreras/:id/materias', existeId.existsById(Carreras),carreraController.getMaterias)
 
-=======
-router.post('/carreras',middlewareCarrera.validaSchema(carreraSchema), carreraController.crearCarrera)
->>>>>>> ac9fa7eff737242778cf888303f5243138618b1d
 
 
 module.exports = router
--- src/routes/profesor.route.js
+++ src/routes/profesor.route.js
@@ -5,9 +5,6 @@ const profesorController = require('../controllers/profesor.controller')
 const existeId = require('../middlewares/exists.middleware')
 const validarSchema = require('../middlewares/validateSchema.middleware')
 const profesorSchema = require('../schemas/profesor.schema')
-
-
-const middlewareProfesor = require('../middleware/existe.middleware')
 const profesorSchema = require('../schemas/profesor.schema')
 
 //Obtenemos todos los profesores
@@ -17,11 +14,7 @@ router.get('/profesores', profesorController.getAllProfesor)
 router.get('/profesores/:id',existeId.existsById(Profesores) ,profesorController.profesorById)
 
 //Creamos un profesor
-<<<<<<< HEAD
 router.post('/profesores',validarSchema.validaSchema(profesorSchema) ,profesorController.crearProfesor)
-=======
-router.post('/profesores',middlewareProfesor.validaSchema(profesorSchema), profesorController.crearProfesor)
->>>>>>> ac9fa7eff737242778cf888303f5243138618b1d
 
 //Modificamos un profesor
 router.put('/profesores/:id',existeId.existsById(Profesores) ,profesorController.modificarProfesor)
